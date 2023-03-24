bf29685676f43151fcd3513384e88f367461f959
Kirill Chivchiev <chiefchief.92@mail.ru>
2023-03-24 22:38:06 +0700

    Add jwt token library

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const { NOT_FOUND_CODE } = require('./utils/constants/status-code');
 // Подключение библиотеки для чтения переменных окружения из .env
 require('dotenv').config();
 
-const { PORT = 3000 } = process.env;
-const { MESTO_DB_CONNECT = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const {
+  PORT = 3000,
+  MESTO_DB_CONNECT = 'mongodb://127.0.0.1:27017/mestodb',
+} = process.env;
 
 const app = express();
 
