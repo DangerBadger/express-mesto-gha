305f8980ae030253cf2951ebff66ae6d2a7bae9b
Chivchiev Kirill <chiefchief.92@mail.ru>
2023-03-30 20:39:08 +0700

    Add token to res body

--- controllers/user.js
+++ controllers/user.js
@@ -120,7 +120,7 @@ module.exports.login = (req, res, next) => {
           httpOnly: true,
           sameSite: true,
         })
-        .send({ message: 'Вы успешно авторизованы' });
+        .send({ token });
     })
     .catch(next);
 };
